90f8b9f5269138517fcaff0c1ae4f0ac9865328f
Hea092024 <Hea092024@gmail.com>
2024-11-18 15:39:53 +0100

    innit commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const myArray = ["apple", "banana", "cherry", "date", "eggplant"];
+
+//1.--------------------------------------------------------------------------------------
+// Print the numbers 1-10 using a "for-loop"
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+//2.--------------------------------------------------------------------------------------
+// Print the numbers 1-10 using a "while loop"
+
+//3.--------------------------------------------------------------------------------------
+// Print each item in myArray using a "for loop".
+
+//4.--------------------------------------------------------------------------------------
+// Using the myArray.push() method, add "mango" to the end of myArray.
+//print myArray using a for... of loop.
+
+//5.--------------------------------------------------------------------------------------
+//Using the myArray.splice() method, remove "cherry" from myArray
+// print the result using myArray.foreach()
+
+//6.--------------------------------------------------------------------------------------
+// Using the .shift() and .pop() method, remove the first and last item from myArray.
+//print the result
+
+// 7.--------------------------------------------------------------------------------------
+// create a variable and assign it the content of myArray as a string using .join().
+// print your variable.
+//BONUS - See if you can make the items be seperated by a space
+
+//8--------------------------------------------------------------------------------------
+//Create a function that takes a number as an argument and returns that number squared.
+// call your function by using a valid argument
+function numberSquared(number) {}
+
+numberSquared();
+
+//9.--------------------------------------------------------------------------------------
+//Using the "%" (modulo) operator complete the function,
+// it should take "number" as an argument and return "even" if its even and "odd" if its odd.
+function oddOrEven(number) {}
+//call your function by using one of the options below.
+// oddOrEven(prompt());
+// oddOrEven()
+
+//10.--------------------------------------------------------------------------------------
+//write a function that prints the fibbonacci sequence for the number passed into it
+// HINT! fibbonacchi =
+//  c = a + b;
+//  a = b;
+//  b = c;
+function Fibbonacci(number) {}
+Fibbonacci();
