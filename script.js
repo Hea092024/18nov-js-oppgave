6023b6d00a46b6bc5ea72ddf5a55bb8152d86ef2
Hea092024 <Hea092024@gmail.com>
2024-11-19 11:31:02 +0100

    .

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const myArray = ["apple", "banana", "cherry", "date", "eggplant"];
 
+
+
+
 //1.--------------------------------------------------------------------------------------
 // Print the numbers 1-10 using a "for-loop"
 
@@ -19,9 +22,9 @@ const myArray = ["apple", "banana", "cherry", "date", "eggplant"];
 //3.--------------------------------------------------------------------------------------
 // Print each item in myArray using a "for loop".
 
-// for (let i = 0; i < myArray.length; i++) {
-//     console.log(myArray[i]);
-// }
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
 
 //4.--------------------------------------------------------------------------------------
 // Using the myArray.push() method, add "mango" to the end of myArray.
@@ -76,15 +79,15 @@ const myArray = ["apple", "banana", "cherry", "date", "eggplant"];
 //Using the "%" (modulo) operator complete the function,
 // it should take "number" as an argument and return "even" if its even and "odd" if its odd.
 
-function oddOrEven(number) {
-    if (number % 7 === 0)
-        return "It is Even Number"; 
-    else {
-        return "It is Odd Number"
-    }
-}
+// function oddOrEven(number) {
+//     if (number % 7 === 0)
+//         return "It is Even Number"; 
+//     else {
+//         return "It is Odd Number"
+//     }
+// }
 
-console.log(oddOrEven());
+// console.log(oddOrEven());
 
 
 
@@ -100,3 +103,22 @@ console.log(oddOrEven());
 //  b = c;
 function Fibbonacci(number) {}
 Fibbonacci();
+
+
+//11.--------------------------------------------------------------------------------------
+//BONUS!Implement fizzbuzz, if a number is divisible by 3: Fizz, if a number is divisible by 5: Buzz, if the number is divisible by both 3 and 5, FizzBuzz
+// https://www.geeksforgeeks.org/fizz-buzz-implementation/
+function fizzbuzz(number) {}
+
+fizzbuzz();
+
+//12.--------------------------------------------------------------------------------------
+//SUPERBONUS - we have not talked about recursion yet, but here is a fun one to try.
+//recursion is when a function calls itself.
+//Complete the function so that it returns the factorial of a number. Factorial (n!) is the product of all whole numbers between 1 and n
+//example: factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+function factorial(n) {
+  if (n === 0) return 1;
+  return; /// din kode her
+}
+console.log(factorial(5));
