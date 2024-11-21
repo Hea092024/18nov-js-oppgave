ce8486c53e2ee6a74296c4f3035049280727b877
Hea092024 <Hea092024@gmail.com>
2024-11-21 10:16:16 +0100

    updated js

--- script.js
+++ script.js
@@ -119,19 +119,20 @@ const myArray = ["apple", "banana", "cherry", "date", "eggplant"];
 //11.--------------------------------------------------------------------------------------
 //BONUS!Implement fizzbuzz, if a number is divisible by 3: Fizz, if a number is divisible by 5: Buzz, if the number is divisible by both 3 and 5, FizzBuzz
 // https://www.geeksforgeeks.org/fizz-buzz-implementation/
-function fizzbuzz(number) {
-  if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (number % 3 === 0) {
-    console.log("Fizz");
-  } else if (number % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(number);
-  }
-}
 
-fizzbuzz(15);
+// function fizzbuzz(number) {
+//   if (number % 3 === 0 && number % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (number % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (number % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(number);
+//   }
+// }
+
+// fizzbuzz(15);
 
 
 //12.--------------------------------------------------------------------------------------
@@ -142,6 +143,6 @@ fizzbuzz(15);
 
 // function factorial(n) {
 //   if (n === 0) return 1;
-//   return; /// din kode her
+//   return n * factorial(n - 1);
 // }
 // console.log(factorial(5));
